06420d878279ad3f76cd4c21466b9026879fff74
Ubuntu <ubuntu@ip-172-31-17-202.us-west-2.compute.internal>
2013-08-06 14:53:01 +0000

    added test connection connect.php

---
+++ connect.php
@@ -0,0 +1,18 @@
+
+<?php 
+   require_once('db.php');
+   if(!$dbconn = mysql_connect (DB_HOST, DB_USER, DB_PW)){
+     echo 'could not connect to mysql on ' . DB_HOST . '\n';
+      exit;
+     } 
+echo 'connected to mysql <br />';
+if (!mysql_select_db(DB_NAME, $dbconn)){
+    echo 'Could not user database ' . DB_NAME . '\n';
+    echo mysql_error() . '\n';
+    exit;
+   }
+
+  echo 'connected to database ' . DB_NAME . '\n';
+?>
+
+
